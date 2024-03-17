34c5c1180e85cf388079d4bcda26001960edf290
Deepak Shenoy <shenoyd@jupiter.local>
2024-03-16 23:32:25 -0400

    Development

--- aircraft-passenger-sizing-simple/aircraft-passenger-sizing-simple.cpp
+++ aircraft-passenger-sizing-simple/aircraft-passenger-sizing-simple.cpp
@@ -20,8 +20,15 @@ int main() {
         // Create the model context
         GRBModel model = GRBModel(env);
 
-        https://www.delta.com/us/en/aircraft/boeing/767-300er
-        GRBVar b767_300 = model.addVar(0, 226, );
+        // Create variables - aircraft
+        // https://www.delta.com/us/en/aircraft/boeing/767-300er
+        GRBVar b767_300er = model.addVar(0, 226, 0, GRB_INTEGER, "b767_300er");
+        // https://www.delta.com/us/en/aircraft/airbus/a350
+        GRBVar a350_900 = model.addVar(0, 360, 0, GRB_INTEGER, "a350_900");
+        // https://www.delta.com/us/en/aircraft/boeing/757-300
+        GRBVar b757_300 = model.addVar(0, 234, 0, GRB_INTEGER, "b757_300");
+        // https://www.delta.com/us/en/aircraft/boeing/737-900er
+        GRBVar b737_900er = model.addVar(0, 180, 0, GRB_INTEGER, "b737_900er");
 
 
 
