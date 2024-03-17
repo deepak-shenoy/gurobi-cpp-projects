38378424a7c4eb3381a603a926cf610c92aff1eb
Deepak Shenoy <shenoyd@jupiter.local>
2024-03-16 22:56:54 -0400

    Development

--- aircraft-passenger-sizing-simple/aircraft-passenger-sizing-simple.cpp
+++ aircraft-passenger-sizing-simple/aircraft-passenger-sizing-simple.cpp
@@ -14,11 +14,22 @@ int main() {
 
     try {
 
+        // Create and environment - local and empty environment
+        GRBEnv env = GRBEnv(true);
+
+        // Create the model context
+        GRBModel model = GRBModel(env);
+
+        https://www.delta.com/us/en/aircraft/boeing/767-300er
+        GRBVar b767_300 = model.addVar(0, 226, );
+
+
+
+
     } catch (GRBException e) {
         std::cout << "Error code: " << e.getErrorCode() << " ";
         std::cout << "Error message: " << e.getMessage() << std::endl;
     }
-
 
     return 0;
 }
